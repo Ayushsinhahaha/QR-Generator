f599556e5ed58cf0c1e926039a9f565c607d52e4
Ayush Sinha <iamayush02@gmail.com>
2022-09-06 00:44:53 +0530

    added code for qrcode

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const app = express();
 const ejs = require("ejs");
 const path = require("path");
 const port = process.env.port || 3000;
+const qrcode = require("qrcode");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,'views'));
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/scan", (req, res) => {
+  const inputText = req.body.text;
+  console.log(inputText);
+  qrcode.toDataURL(inputText,(err,src)=>{
+    res.render('scan',{
+        qrcode:src,
+    })
+  });
 });
 
 app.listen(port, () => {
