af7264c2b1e089ffc924b57c506d5c298d5e32b5
Ayush Sinha <iamayush02@gmail.com>
2022-09-06 00:17:44 +0530

    Added Bootstrap and form for the qr

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express=require('express');
-const app=express();
-const ejs=require('ejs');
-const path=require('path');
-const port=process.env.port||3000;
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const port = process.env.port || 3000;
 
-app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,'views'));
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
